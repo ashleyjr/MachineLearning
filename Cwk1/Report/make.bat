b0add6b45e26137621284cffee458b36b89e47ca
Ashley Robinson <ajr2g10@ecs.soton.ac.uk>
2014-02-26 18:28:10 +0000

    more refs

--- Cwk1/Report/make.bat
+++ Cwk1/Report/make.bat
@@ -2,7 +2,7 @@ pdflatex report.tex
 bibtex report
 pdflatex report.tex 
 pdflatex report.tex 
-texcount -v -html -inc Subroutines.tex > count.html
+texcount -v -html -inc report.tex > count.html
 del .pdf
 del *.aux
 del *.bbl
