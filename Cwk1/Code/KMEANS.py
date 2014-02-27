4e2d29e4bdf009fc798476a925cad0588f650ab8
Ashley Robinson <ajr2g10@ecs.soton.ac.uk>
2014-02-27 21:37:41 +0000

    more

--- Cwk1/Code/KMEANS.py
+++ Cwk1/Code/KMEANS.py
@@ -1,5 +1,5 @@
 import numpy as np
-from sklearn.cluster import DBSCAN
+from sklearn.cluster import KMeans
 from sklearn import metrics
 from sklearn.datasets.samples_generator import make_blobs
 from sklearn.preprocessing import StandardScaler
@@ -21,22 +21,20 @@ for line in f:
 X = StandardScaler().fit_transform(X)
 
 ##############################################################################
-# DBSCAN Parameters
-eps = 0.2
-min_samples = 1
+# K-means Parameters
+num = 7
 
 ##############################################################################
 # Compute DBSCAN
-db = DBSCAN(eps, min_samples).fit(X)
-core_samples = db.core_sample_indices_
+db = KMeans(init='random',n_clusters=num).fit(X)
 labels = db.labels_
-
 # Number of clusters in labels, ignoring noise if present.
 n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
 
-
 ##############################################################################
 # Plot result
+# Black removed and is used for noise instead.
+#fig = fig + 1
 pl.figure(1)
 unique_labels = set(labels)
 colors = pl.cm.Spectral(np.linspace(0, 1, len(unique_labels)))
@@ -46,17 +44,10 @@ for k, col in zip(unique_labels, colors):
         col = 'k'
         markersize = 6
     class_members = [index[0] for index in np.argwhere(labels == k)]
-    cluster_core_samples = [index for index in core_samples
-                            if labels[index] == k]
     for index in class_members:
         x = X[index]
-        if index in core_samples and k != -1:
-            markersize = 14
-        else:
-            markersize = 6
         pl.plot(x[0], x[1], 'o', markerfacecolor=col,
                 markeredgecolor='k')
-#pl.title('DBSCAN - %d Clusters' % n_clusters_)
 pl.grid()
 pl.show()
 raw_input("Press Enter to continue...")
--- Cwk1/Code/Samples/DBSCAN.py
+++ Cwk1/Code/Samples/DBSCAN.py
@@ -1,43 +1,46 @@
+print(__doc__)
+
 import numpy as np
+
 from sklearn.cluster import DBSCAN
 from sklearn import metrics
 from sklearn.datasets.samples_generator import make_blobs
 from sklearn.preprocessing import StandardScaler
-import pylab as pl
-pl.ion()
+
 
 ##############################################################################
-# Load data
-f = open('Datasets/Aggregation.txt','r')
-X = []
-labels_true = []
-for line in f:
-	x = line.split('\t')[0]
-	y = line.split('\t')[1]
-	label = line.split('\t')[0]
-	label = label.replace('\n','')
-	X.append([x,y])
-	labels_true.append(label)
+# Generate sample data
+centers = [[10, 10], [-5, -5], [1, -1]]
+X, labels_true = make_blobs(n_samples=1000, centers=centers, cluster_std=0.4,
+                            random_state=0)
+
 X = StandardScaler().fit_transform(X)
 
 ##############################################################################
-# DBSCAN Parameters
-eps = 0.2
-min_samples = 1
-
-##############################################################################
 # Compute DBSCAN
-db = DBSCAN(eps, min_samples).fit(X)
+db = DBSCAN(eps=0.3, min_samples=10).fit(X)
 core_samples = db.core_sample_indices_
 labels = db.labels_
 
 # Number of clusters in labels, ignoring noise if present.
 n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
 
+print('Estimated number of clusters: %d' % n_clusters_)
+print("Homogeneity: %0.3f" % metrics.homogeneity_score(labels_true, labels))
+print("Completeness: %0.3f" % metrics.completeness_score(labels_true, labels))
+print("V-measure: %0.3f" % metrics.v_measure_score(labels_true, labels))
+print("Adjusted Rand Index: %0.3f"
+      % metrics.adjusted_rand_score(labels_true, labels))
+print("Adjusted Mutual Information: %0.3f"
+      % metrics.adjusted_mutual_info_score(labels_true, labels))
+print("Silhouette Coefficient: %0.3f"
+      % metrics.silhouette_score(X, labels))
 
 ##############################################################################
 # Plot result
-pl.figure(1)
+import pylab as pl
+
+# Black removed and is used for noise instead.
 unique_labels = set(labels)
 colors = pl.cm.Spectral(np.linspace(0, 1, len(unique_labels)))
 for k, col in zip(unique_labels, colors):
@@ -55,9 +58,8 @@ for k, col in zip(unique_labels, colors):
         else:
             markersize = 6
         pl.plot(x[0], x[1], 'o', markerfacecolor=col,
-                markeredgecolor='k')
-#pl.title('DBSCAN - %d Clusters' % n_clusters_)
-pl.grid()
+                markeredgecolor='k', markersize=markersize)
+
+pl.title('Estimated number of clusters: %d' % n_clusters_)
 pl.show()
-raw_input("Press Enter to continue...")
 
