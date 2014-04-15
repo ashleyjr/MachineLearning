de01f946f9d7700982201a38c541b04e0b27f6f9
Ashley Robinson <ashley181291@gmail.com>
2014-04-15 17:00:58 +0100

    remove sample

--- Cwk2/Sample_code/ajr_feat_select.m
+++
@@ -1,4 +0,0 @@
-function idx=ajr_feat_select(X, Y, num)
-	fval=Y'*X;
-	[sval, si]=sort(-fval);
-	idx=si(1:num);
--- Cwk2/Sample_code/ajr_predict.m
+++
@@ -1,4 +0,0 @@
-function [Y_resu, Y_conf] = ajr_predict(X_test, param, idx_feat, X_train, Y_train)
-	Y_score=X_test(:,idx_feat)*param.W'+param.b;
-	Y_resu=sign(Y_score);
-	Y_conf=abs(Y_score);
