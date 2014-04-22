7a0b0c92f6db9d873e728f373ab17d8219fb67fc
Ashley Robinson <ajr2g10@ecs.soton.ac.uk>
2014-04-22 01:12:04 +0100

    train

--- Cwk2/MLP/train.m
+++ Cwk2/MLP/train.m
@@ -1,38 +1,38 @@
 function [param,feat_out]=train(X_train, Y_train, X_valid, Y_valid, feat_in)
+	eta=1;
 	X=X_train(:,feat_in);						% Subset of features
 	patterns=size(X,1);
 	features=size(X,2);
-	param.w_in=rand(features,features)-0.5;		% Weights from input to each hidden layer node
-	param.w_out=rand(1,features,1)-0.5;			% Weights from hidden layer to output 
-	param.b_in = 0;
-	param.b_out = 0;
+	ones_row=ones(patterns,1);
+	X=horzcat(X,ones_row);					% Offset on all input patterns
+	w_in=ones(features,features+1)-0.5;		% Weights from input to each hidden layer node
+	w_out=rand(1,features+1,1)-0.5;			% Weights from hidden layer to output 
 
-	minErr = 1;
 
-	for i=1:5000	
+	rec = 1
+	for i=1:100	
 		disp(i)
-		w_in_move=10*(rand(features,features)-0.5);		% Weights from input to each hidden layer node
-		w_out_move=10*(rand(1,features)-0.5);			% Weights from hidden layer to output 
-		b_in_move = rand()-0.5;
-		b_out_move = rand()-0.5;
 
-		param.w_in = param.w_in + w_in_move;
-		param.w_out = param.w_out + w_out_move;
-		param.b_in = param.b_in + b_in_move;
-		param.b_out = param.b_out + b_out_move;
-
+		param.w_out = w_out(1:(end-1));
+		param.b_out = w_out(end);
+		param.w_in = w_in(:,1:(end-1));
+		param.b_in = mean(w_in(:,end));
 		
 		[resu, conf] = predict(X_train,param,feat_in);
-		err(i)=balanced_errate(resu, Y_train);
-		
-		if(err(i)<minErr)
-			minErr = err(i);
-		else
-			param.w_in = param.w_in - w_in_move;
-			param.w_out = param.w_out - w_out_move;
-			param.b_in = param.b_in - b_in_move;
-			param.b_out = param.b_out - b_out_move;
-		end
+
+		deltaW = eta*((Y_train - resu)'*X);
+		w_out = w_out - deltaW;
+
+		err(rec)=balanced_errate(resu, Y_train);
+		rec = rec + 1;
+
+		%for j=1:features
+		%	[resu, conf] = predict(X_train,param,feat_in);	
+		%	deltaW = eta*((Y_train - resu)'*X);
+		%	w_in(j,:) = w_in(j,:) - deltaW;
+		%	err(rec)=balanced_errate(resu, Y_train);
+		%	rec = rec + 1;
+		%end
 	end
 
 	figure;
