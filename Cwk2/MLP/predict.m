aa5764ca16809ba53681cbed9c8c285638faa043
Ashley Robinson <ajr2g10@ecs.soton.ac.uk>
2014-04-24 03:33:14 +0100

    mlp works

--- Cwk2/MLP/predict.m
+++ Cwk2/MLP/predict.m
@@ -1,23 +1,10 @@
 function [Y_resu, Y_conf] = predict(X_test, param, feat)	
 	X=X_test(:,feat);
-	patterns=size(X,1);
-	features=size(X,2);
+	hiddens=size(param.w_in,1)
 
-	hidden=zeros(features,1);
-	Y_score=zeros(patterns,1);
-
-	
-	for i=1:patterns
-		%	for j=1:features
-		%		for k=1:features
-		%			hidden(j) = hidden(j) + param.w_in(j,k)*X(i,j);
-		%		end
-		%	end
-		%	for j=1:features
-		%		Y_score(i) = Y_score(i) + param.w_out(j).*hidden(j);		
-		%	end
-		
-		Y_score(i) = param.w_out*(tanh(param.w_in*X(i,:)')+param.b_in)+param.b_out;
+	for j=1:hiddens
+		Y_hidden(j,:) = tanh(X*param.w_in(j,:)'+param.b_in(j));
 	end
+	Y_score = unitVec(Y_hidden'*param.w_out')+param.b_out;
 	Y_resu=unitVec(Y_score)';	
-	Y_conf=abs(Y_score);
+	Y_conf=abs(Y_score)';
--- Cwk2/MLP/train.m
+++ Cwk2/MLP/train.m
@@ -1,76 +1,69 @@
 function [param,feat_out]=train(X_train, Y_train, X_valid, Y_valid, feat_in)
-	eta=1e-6;
+	eta=1e-5;
 	X=X_train(:,feat_in);						% Subset of features
-	x_val=X_valid(:,feat_in);
+	X_val=X_valid(:,feat_in);
 	patterns=size(X,1);
 	ones_row=ones(patterns,1);
 	X=horzcat(X,ones_row);					% Offset on all input patternus
-	x_val=horzcat(x_val,ones_row);					% Offset on all input patterns
+	X_val=horzcat(X_val,ones_row);
 	features=size(X,2);
-	w_in=ones(features,features)-0.5;		% Weights from input to each hidden layer node
-	w_out=ones(1,features,1)-0.5;			% Weights from hidden layer to output 
+	hiddens=features;
+	w_in=rand(hiddens,features)-0.5;		% Weights from input to each hidden layer node
+	w_out=rand(1,hiddens);			% Weights from hidden layer to output 
 	param.b_out = 0;
 	param.b_in = 0;
 
 
-	for j=1:patterns
-		Y(j) = w_out*(tanh(w_in*X(j,:)'));
-	end
-	Y = unitVec(Y);
 	
-	for i=1:600
+	for i=1:500
 		disp(i)
 
-		
-		% AJR - Use tanh to feedback error?, don;t use _|- 
 
-
-
-		% Output layer
-		delta_out = (Y_train' - Y);	
-		w_new_out = w_out + eta*delta_out*X;
-
-
-		% Hidden layer
-		for k=1:features
-			delta_in = w_out(k)*delta_out;
-			w_in(k,:) = w_in(k,:) + eta*delta_in'*X;
+		% Forward
+		for j=1:hiddens	
+			Y_hidden(j,:) = tanh(X*w_in(j,:)');
 		end
+		Y = tanh(Y_hidden'*w_out')';
 
-		w_out = w_new_out;
+
+
+		% BackProp
+		E = (Y_train' - Y);
+		delta_out = eta.*E*X;
+		for j=1:hiddens
+			w_in(j,:) = w_in(j,:) + w_out(j)*delta_out;
+		end
+		w_out = w_out + delta_out;
+
 
 
 
 		% performace
-		param.w_out = w_out(1,1:(end-1));
-		param.b_out = w_out(end);
-		param.w_in = w_in(1:(end-1),1:(end-1));
-		param.b_in = w_in(end,1:(end-1))';
 
 
-    	%[Y_resu_valid, Y_conf_valid] 	= predict( X_valid, param,	feat_in	);
-		%[Y_resu_train, Y_conf_train] 	= predict( X_train, param,	feat_in	);
-		
-		for j=1:patterns
-			Y(j) = w_out*(tanh(w_in*X(j,:)'));
+		for j=1:hiddens	
+			Y_hidden(j,:) = tanh(X_val*w_in(j,:)');
 		end
-		Y = unitVec(Y);
-
-		for j=1:patterns
-			y_val(j) = w_out*(tanh(w_in*x_val(j,:)'));
+		Y_val = unitVec(Y_hidden'*w_out');
+		% Forward
+		for j=1:hiddens	
+			Y_hidden(j,:) = tanh(X*w_in(j,:)');
 		end
-		y_val = unitVec(y_val);
-
-
-		err_valid(i)=balanced_errate(y_val', Y_valid);
+		Y = unitVec(Y_hidden'*w_out');
 		err_train(i)=balanced_errate(Y', Y_train);
+		err_valid(i)=balanced_errate(Y_val', Y_valid);
+
 		x(i) = i;
 	end
 
 
 
 	figure;
-	plot(x,err_train,x,err_valid)
+	plot(x,err_train,x,err_valid);
 
-	
+	param.w_in = w_in(:,(1:(end-1)));
+	param.b_in = w_in(:,end);
+	param.w_out = w_out;
+	param.b_out = 0;
+
 	feat_out = feat_in;
